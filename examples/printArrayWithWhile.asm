cede337b1728227680850c9f7f3be4a93f747446
aEmreAydemir <45498427+aEmreAydemir@users.noreply.github.com>
2021-04-12 08:10:45 +0300

    array is predefined

--- examples/printArrayWithWhile.asm
+++ examples/printArrayWithWhile.asm
@@ -1,26 +1,15 @@
 .data
-	array: .space 12 #allocate 4bytes for each integer. allocated space for 3 integers  ######## INSIDE RAM
+	array: .word 3 5 12 53 24 # 5 elements declared. if i did like 3:5 . it would put 3 in all 5 indexes
 	newLine: .asciiz "\n"
 .text
 	main:
-	addi $s0,$zero,4
-	addi $s1,$zero , 11	########### INSIDE PROCESSOR
-	addi $s2,$zero, 22
-	
-	# index /offset   =$t0
-	addi $t0,$zero,0 # just for index
-	
-	sw $s0,array($t0) #put the s0 in first 4 bytes of array
-	addi $t0,$t0,4 	#increment index by 4 because int is 4 bytes
-	sw $s1,array($t0)
-	addi $t0,$t0,4
-	sw $s2,array($t0)
+
 	
 	#clear $t0 to 0
 	addi $t0,$zero,0
 	
 	while:
-		beq $t0,12,exit
+		beq $t0,20,exit
 		
 		lw $t6,array($t0)
 		li $v0,1
